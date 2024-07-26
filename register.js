c6c55f14cb277d9219521998fec80cbd50c3ebd8
talhahzahid <talhazahid415@gmail.com>
2024-07-27 00:36:09 +0500

    autentication

---
+++ register.js
@@ -0,0 +1,21 @@
+
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { auth } from "./firebaseconfig.js";
+
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+form.addEventListener( "submit", (event) => {
+  event.preventDefault();
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      window.location = './login.html'
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
